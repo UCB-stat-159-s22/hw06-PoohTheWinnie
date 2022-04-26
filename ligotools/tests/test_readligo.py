aea5b65ef28b84be8cdc7ea0d1a3414fe3e2e7da
PoohTheWinnie <winston.x.cai@gmail.com>
2022-04-26 00:56:08 +0000

    Finished JupyterBook

--- ligotools/tests/test_readligo.py
+++ ligotools/tests/test_readligo.py
@@ -1,22 +1,22 @@
-from ligotools.readligo import loaddata, read_frame
-from ligotools.readligo import FileList
-
+# from ligotools.readligo import loaddata, read_frame
+# from ligotools.readligo import FileList
+from ligotools import readligo as rl
 
 def test_FileList():
-    files = FileList()
+    files = rl.FileList()
     assert len(files.searchdir('data/')) == 10
 
 def test_loaddata_file_one():
     file = 'data/H-H1_LOSC_4_V2-1135136334-32.hdf5'
-    output = loaddata(file, 'H1')
+    output = rl.loaddata(file, 'H1')
     assert len(output[0]) == len(output[1])
     
 def test_loaddata_file_two():
     file = 'data/H-H1_LOSC_4_V2-1126259446-32.hdf5'
-    output = loaddata(file, 'H1')
+    output = rl.loaddata(file, 'H1')
     assert len(output[0]) == len(output[1])
     
 def test_loaddata_file_three():
     file = 'data/L-L1_LOSC_4_V2-1128678884-32.hdf5'
-    output = loaddata(file, 'L1')
+    output = rl.loaddata(file, 'L1')
     assert len(output[0]) == len(output[1])
